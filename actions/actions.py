9f969018719d58278eaca6718da5d1f3bd42b394
michael22ray21 <michael22ray21@yahoo.com>
2021-08-22 11:18:10 +0700

    milestone seminar 2 done

--- actions/actions.py
+++ actions/actions.py
@@ -7,13 +7,42 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
+# import datetime
 from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 from rasa_sdk.events import SlotSet
+# ReminderScheduled
 
+""" class ActionSetReminder(Action):
+    Schedules a reminder, supplied with the last message's entities.
+
+    def name(self) -> Text:
+        return "action_set_reminder"
+
+    async def run(
+        self,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: Dict[Text, Any],
+    ) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message("I will remind you in 5 seconds.")
+
+        date = datetime.datetime.now() + datetime.timedelta(seconds=5)
+        entities = tracker.latest_message.get("entities")
+
+        reminder = ReminderScheduled(
+            "EXTERNAL_reminder",
+            trigger_date_time=date,
+            entities=entities,
+            name="my_reminder",
+            kill_on_user_message=False,
+        )
+
+        return [reminder] """
 
 class ActionHelloWorld(Action):
 
